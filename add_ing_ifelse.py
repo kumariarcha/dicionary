efa5a3b8c8db962428a43c6950edb65e48a214b8
Archana singh <95541918+kumariarcha@users.noreply.github.com>
2022-07-11 17:06:03 +0530

    ing_ifelse.py

---
+++ add_ing_ifelse.py
@@ -0,0 +1,32 @@
+# i=1
+# while i<=100:
+#     print(i)
+#     i=i+1
+#     if i==50:
+#         continuei
+
+# i=1
+# while i<=5:
+#     print("*"*i)
+#     i=i+1
+
+
+# user=str(input("enter the word"))
+# if "ing" not in user:
+#     print(user+"ing"+"ly")
+# # elif "ing" in user:
+# #     print(user+"ly"+)
+# else:
+#     print(user)
+
+
+# list=[[1,2],[3,4],[5,6],[7,8]]
+# i=0
+# while i<len(list):
+#     j=0
+#     list2=[]
+#     while j<len(list[i])+2:
+#         list2.append(sum(list[j]))
+#         j=j+1
+#     i=i+1
+# print(list2)
